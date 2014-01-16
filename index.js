c3b480aa072efa564117afb6a24eee2755749639
Alfred Dobradi <harvey@thebirdcave.com>
2014-01-16 12:34:07 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.parse = function(postContent, callback) {
+    postContent = postContent.replace(/\[\[spotify (track|album)=([a-zA-Z0-9]+)\]\]/g,'<iframe src="https://embed.spotify.com/?uri=spotify:$1:$2" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>');
+    callback(null,postContent);
+};
